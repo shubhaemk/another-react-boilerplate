29503739843bed8428487967102ac43973394847
Shubham Kamath <dev.shubhaem@gmail.com>
2019-12-01 14:21:34 +0530

    testing

--- src/components/dummy/dummy.jsx
+++ src/components/dummy/dummy.jsx
@@ -1,9 +1,13 @@
 import React,{Component} from 'react';
-
+import Dummy2 from './dummy2';
 class Dummy extends Component{
     render(){
         return(
-            <h1>This is dummy</h1>
+            <div>
+                <h1>This is dummy</h1>
+                <Dummy2></Dummy2>
+            </div>
+            
         )
     }
 };
---
+++ src/components/dummy/dummy2.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class Dummy2 extends Component {
+    render() {
+        return (
+            <h1>This is dummy2</h1>
+        )
+    }
+};
+
+export default Dummy2;
