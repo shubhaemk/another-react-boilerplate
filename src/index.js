430710c359687b265c43e1f73f5e8df9baa2041f
Shubham Kamath <dev.shubhaem@gmail.com>
2019-12-02 00:51:28 +0530

    testing file-loader

--- src/index.js
+++ src/index.js
@@ -2,11 +2,17 @@ import React from "react";
 import ReactDOM from "react-dom";
 
 import Dummy from './components/dummy/dummy';
-
+import img from './assets/img.jpg';
 
 const App = () => {
-    console.log(process.cwd())
-    return (<div>Hello <Dummy></Dummy>React,Webpack 4 & Babel 7!</div>);
+    return (
+        <div>
+            Hello 
+            <img src={img}></img>
+            <Dummy></Dummy>
+            React,Webpack 4 & Babel 7!
+        </div>
+    );
 };
 
 ReactDOM.render(<App />, document.getElementById("root"));
