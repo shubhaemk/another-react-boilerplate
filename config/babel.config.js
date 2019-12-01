b1b485306d2f330ef2800bd1161af63afd4092b8
Shubham Kamath <dev.shubhaem@gmail.com>
2019-12-01 12:42:38 +0530

    this is finally working!

---
+++ config/babel.config.js
@@ -0,0 +1,13 @@
+const path = require('path');
+
+/*
+"@babel/preset-env",
+'@babel/preset-react'
+*/
+module.exports = {
+    presets:[
+        "@babel/preset-env",
+        '@babel/preset-react'
+    ]
+};
+
