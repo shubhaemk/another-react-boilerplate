7d9f4b78f05768c38a85195656185ef82616bf39
Shubham Kamath <dev.shubhaem@gmail.com>
2019-12-14 18:52:13 +0530

    added class component

--- src/app.jsx
+++ src/app.jsx
@@ -6,19 +6,36 @@ import img3 from './images/img3.png';
 import './index.css';
 import './main.scss';
 
-const App = () => {
-    return (
-        <div>
-            Hello
+class App extends React.Component {
+
+    constructor(props){
+        super(props);
+        this.state = {
+            var : '2'
+        }
+    }
+
+    componentDidMount = () => {
+        this.setState({
+            var: '3'
+        },
+        console.log(this.state.var))
+    }
+
+    render(){
+        return (
+            <div>
+                Hello
             <img src={img1}></img>
-            <img src={img2}></img>
-            <img src={img3}></img>
-            React,Webpack 4 & Babel 7!
-            {[1, 2, 3, 5].map((x) =>{
-                return x+1
-            } )}
-        </div>
-    );
-};
+                <img src={img2}></img>
+                <img src={img3}></img>
+                React,Webpack 4 & Babel 7!
+            {[1, 2, 3, 5].map((x) => {
+                    return x + 1
+                })}
+            </div>
+        );
+    }
+}
 
 export default App;
