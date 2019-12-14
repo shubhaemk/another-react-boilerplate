3b2d7016cdbe45a84011fead17a021038fb8aa04
Shubham Kamath <dev.shubhaem@gmail.com>
2019-12-14 14:42:05 +0530

    testing media and css

--- src/index.js
+++ src/index.js
@@ -1,9 +1,21 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
+
+import img1 from './images/img1.png';
+import img2 from './images/img2.png';
+import img3 from './images/img3.png';
+
+import './index.css';
+import './main.scss';
+
 const App = () => {
     return (
         <div>
+            Hello 
+            <img src={img1}></img>
+            <img src={img2}></img>
+            <img src={img3}></img>
             React,Webpack 4 & Babel 7!
         </div>
     );
