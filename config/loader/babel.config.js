190970034de70ce2def2bb5963d0d871b9b0058b
Shubham Kamath <dev.shubhaem@gmail.com>
2019-12-14 17:05:32 +0530

    added seperate loader configs

---
+++ config/loader/babel.config.js
@@ -0,0 +1,14 @@
+/*
+    ToDo
+    1. Return a function instead
+    2. Configure env to support browsers
+    3. Check for react configuration
+*/ 
+
+module.exports = {
+    presets:[
+        "@babel/preset-env",
+        '@babel/preset-react'
+    ]
+};
+
