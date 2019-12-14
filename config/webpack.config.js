c629f179f185b045fb3a97f010ea416c09b51483
Shubham Kamath <dev.shubhaem@gmail.com>
2019-12-14 11:24:36 +0530

    works on different modes

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -1,34 +1,10 @@
-/*
-    ToDo
-    1.Add support for following
-        a. SASS
-        b. CSS
-        c. images/music/gif                                         DONE
-    2. minify css/html/js
-    3. Convert it into a function which returns config              DONE 
-    4. Create different config for production and development
-    5. When a user comes to the website then browser will download the whole bundle file and then render the website. This can slow your website drastically so code splitting allows the generated to split and create multiple outputs. So the browser will load the appropriate bundle when it needs it. In turn improving the site load up time.
-    6. ESLint and pretty quick
-*/
+const webpackProductionConfig = require('./webpack/production.config.js');
+const webpackDevelopmentConfig = require('./webpack/development.config.js');
 
-
-
-
-const webpackModule = require('./webpackConfig/webpackModule.config');
-const webpackPlugins = require('./webpackConfig/webpackPlugins.config');
-const webpackDevServer = require('./webpackConfig/webpackDevServer.config');
-const {fileResolve,pathResolve} = require('./helper/path');
-
-module.exports = {
-    entry: fileResolve('src/index.js'),
-    output: {
-        path: pathResolve('build'),
-        filename: 'index.js',
-    },
-    module: webpackModule(),
-    resolve: {
-        extensions: [".wasm", ".mjs", ".js", ".json", ".jsx"]
-    },
-    plugins: webpackPlugins(),
-    devServer : webpackDevServer(pathResolve('public')),
-};
+module.exports = function(env,argv) {
+    if(argv.mode === 'production'){
+        return webpackProductionConfig();
+    }else{
+        return webpackDevelopmentConfig();
+    }
+}
