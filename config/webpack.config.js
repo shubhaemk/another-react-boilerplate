4b058a9707e428c3951fa33aab13577516aecc3d
Shubham Kamath <dev.shubhaem@gmail.com>
2019-12-15 13:35:29 +0530

    testing chalk

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -1,7 +1,11 @@
 const webpackProductionConfig = require('./webpack/production.config.js');
 const webpackDevelopmentConfig = require('./webpack/development.config.js');
+const chalk = require('chalk');
 
 module.exports = function(env,argv) {
+    console.log(chalk.red.bold('Starting another-react-boilerplate'));
+    console.log(`${chalk.red('Contribute -> ')} ${chalk.red.underline('https://github.com/shubhaemk/another-react-boilerplate')}`);
+    console.log(`${chalk.red('ℹ')} ${chalk.white.dim('｢arb｣')}: Using ${argv.mode}`);
     if(argv.mode === 'production'){
         return webpackProductionConfig();
     }else{
