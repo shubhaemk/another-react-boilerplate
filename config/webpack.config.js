0835f9e0c93100df143d4c8891e5b7a3807134e5
Shubham Kamath <dev.shubhaem@gmail.com>
2019-12-15 16:59:20 +0530

    added instruction at start

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -1,11 +1,18 @@
 const webpackProductionConfig = require('./webpack/production.config.js');
 const webpackDevelopmentConfig = require('./webpack/development.config.js');
-const chalk = require('chalk');
+const { printEmpty, printTitle, printInfo, printMessage } = require('./helper/console.messages.js');
+const { webpackFolder, loaderFolder, pathResolve } = require('./helper/path');
 
 module.exports = function(env,argv) {
-    console.log(chalk.red.bold('Starting another-react-boilerplate'));
-    console.log(`${chalk.red('Contribute -> ')} ${chalk.red.underline('https://github.com/shubhaemk/another-react-boilerplate')}`);
-    console.log(`${chalk.red('ℹ')} ${chalk.white.dim('｢arb｣')}: Using ${argv.mode}`);
+    printTitle(argv.mode);
+    printEmpty();
+    printInfo('Author ->', 'Shubham Kamath');
+    printInfo('Contribute/Star -> ', 'https://github.com/shubhaemk/another-react-boilerplate');
+    printEmpty();
+    printInfo(`Webpack Config (dev/prod) -> `, pathResolve(webpackFolder));
+    printInfo(`Babel, ESLint (dev/prod) -> `, pathResolve(loaderFolder));
+    printEmpty();
+    
     if(argv.mode === 'production'){
         return webpackProductionConfig();
     }else{
