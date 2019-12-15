d2d2dc16a31d2567cc285023ce811bdb961d186a
Shubham Kamath <dev.shubhaem@gmail.com>
2019-12-15 13:36:00 +0530

    refactored functions and added common file paths

--- config/helper/path.js
+++ config/helper/path.js
@@ -1,11 +1,31 @@
 const path = require('path');
 
-const fileResolve = (fileRelativePath) => path.resolve(fileRelativePath);
-const moduleResolve = (moduleRelativePath) => path.resolve('node_modules',moduleRelativePath);
-const pathResolve = (relativePath) => path.resolve(relativePath);
+const pathResolve = (...relativePath) => path.resolve(...relativePath);
+//development
+const hostFolder = 'host';
+//production
+const buildFolder = 'build';
+//common
+const indexJsFile = 'src/index.js';
+const nodeModulesFolder = 'node_modules';
+const babelConfigFile = 'config/loader/babel.config.js';
+const eslintConfigFile = 'config/loader/.eslintrc';
+const assetsFolder = 'assets';
+const publicFolder = 'public';
+const indexHtmlFile = 'index.html';
+const faviconFile = 'favicon.ico';
+
 
 module.exports = {
-    fileResolve,
-    moduleResolve,
-    pathResolve
-}
+    pathResolve,
+    indexJsFile,
+    hostFolder,
+    buildFolder,
+    nodeModulesFolder,
+    babelConfigFile,
+    eslintConfigFile,
+    assetsFolder,
+    publicFolder,
+    indexHtmlFile,
+    faviconFile
+};
